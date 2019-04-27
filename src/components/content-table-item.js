8adf0dcbefb885579c9773a2b3346e8c2dc47db4
Denis <veenrock@gmail.com>
2019-04-27 20:05:41 +0400

    Add table item component

--- src/components/content-table-item.js
+++ src/components/content-table-item.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ApiService from '../services/api-service';
+export default class ContentTableItem extends Component {
+  
+  apiService = new ApiService();
 
-const ContentTableItem = () => {
-  return (
-  <span>12564</span>
-  );
+  state = {
+    id: null,
+    city: null,
+    text: null,
+    date: null,
+    cash: null
+  };
+
+  updateTask() {
+    this.apiService
+      .getNewTask()
+      .then((task) => {
+        this.setState({
+          id: task.id,
+          city: task.city,
+          text: task.text,
+          date: task.date,
+          cash: task.cash
+        });
+      });
+  };
+
+  constructor() {
+    super();
+    this.updateTask();
+  }
+
+
+  render() {
+
+    const { id, city, text, date, cash } = this.state;
+
+    return (
+    <tr>
+      <td>{id}</td>
+      <td>{city}</td>
+      <td>{text}</td>
+      <td>{date}</td>
+      <td>{cash}</td>
+    </tr>
+    );
+  }
 };
-
-export default ContentTableItem;
--- src/components/content-table.js
+++ src/components/content-table.js
@@ -1,85 +1,44 @@
-import React, { Component } from 'react';
-import ApiService from '../services/api-service'
-
+import React from 'react';
 import ContentTableItem from './content-table-item';
 
-export default class ContentTable extends Component {
-
-  apiService = new ApiService();
-
-  state = {
-    id: null,
-    city: null,
-    text: null,
-    date: null,
-    cash: null
-  };
-
-  updateTask() {
-    this.apiService
-      .getNewTask()
-      .then((task) => {
-        this.setState({
-          id: task.id,
-          city: task.city,
-          text: task.text,
-          date: task.date,
-          cash: task.cash
-        });
-      });
-  };
-
-  constructor() {
-    super();
-    this.updateTask();
-  }
-
-  render() {
-
-    const { id, city, text, date, cash } = this.state;
-
-    return (
-      <div className="container content__table">
-      <table>
-        <tbody>
-        <tr>
-          <th>ID</th>
-          <th>Город</th>
-          <th>Название</th>
-          <th>Выполнить до</th>
-          <th>Сумма</th>
-        </tr>
-        <tr>
-          <td><ContentTableItem /></td>
-          <td>Самара</td>
-          <td>Подключение провода от клеммной коробки пожарной сигнализации к расцепителю, проверить работоспособность</td>
-          <td>13 августа</td>
-          <td>7 021 ₽</td>
-        </tr>
-        <tr>
-          <td>12565</td>
-          <td>Волгоград</td>
-          <td>Установить систему видео-наблюдения с возможностью осуществлять on-line просмотр видеоряда с камер</td>
-          <td>13 августа</td>
-          <td>27 340 ₽</td>
-        </tr>
-        <tr>
-          <td>12566</td>
-          <td>Челябинск</td>
-          <td>Заменить осветительное оборудование в складском помещении со смешанным типом хранения</td>
-          <td>13 августа</td>
-          <td>12 780 ₽</td>
-        </tr>
-        <tr>
-          <td>{id}</td>
-          <td>{city}</td>
-          <td>{text}</td>
-          <td>{date}</td>
-          <td>{cash}</td>
-        </tr>
-        </tbody>
-      </table>
-      </div>
-    );
-  }
+const ContentTable = () => {
+  return (
+    <div className="container content__table">
+    <table>
+      <tbody>
+      <tr>
+        <th>ID</th>
+        <th>Город</th>
+        <th>Название</th>
+        <th>Выполнить до</th>
+        <th>Сумма</th>
+      </tr>
+      <tr>
+        <td>12564</td>
+        <td>Самара</td>
+        <td>Подключение провода от клеммной коробки пожарной сигнализации к расцепителю, проверить работоспособность</td>
+        <td>13 августа</td>
+        <td>7 021 ₽</td>
+      </tr>
+      <tr>
+        <td>12565</td>
+        <td>Волгоград</td>
+        <td>Установить систему видео-наблюдения с возможностью осуществлять on-line просмотр видеоряда с камер</td>
+        <td>13 августа</td>
+        <td>27 340 ₽</td>
+      </tr>
+      <tr>
+        <td>12566</td>
+        <td>Челябинск</td>
+        <td>Заменить осветительное оборудование в складском помещении со смешанным типом хранения</td>
+        <td>13 августа</td>
+        <td>12 780 ₽</td>
+      </tr>
+      <ContentTableItem />
+      </tbody>
+    </table>
+    </div>
+  );
 };
+
+export default ContentTable;
