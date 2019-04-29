024fc46b7761d9796f361de33854447568be6257
Denis <veenrock@gmail.com>
2019-04-29 14:25:32 +0400

    Add to content menu <a> color style

--- src/components/content-menu.js
+++ src/components/content-menu.js
@@ -6,11 +6,11 @@ const ContentMenu = () => {
       <div className="content__menu container">
         <ul >
           <li className="content__menu__chosen"><a href="/">Новые</a><span className="content__menu__span">1</span></li>
-          <li>В работе<span className="content__menu__orange">1</span></li>
-          <li>На рассмотрении<span className="content__menu__span">3</span></li>
-          <li>Выполнены<span className="content__menu__span">187</span></li>
-          <li>Отменены<span className="content__menu__span">4</span></li>
-          <li>Черновики<span className="content__menu__span">7</span></li>
+          <li><a href="/">В работе</a><span className="content__menu__orange">1</span></li>
+          <li><a href="/">На рассмотрении</a><span className="content__menu__span">3</span></li>
+          <li><a href="/">Выполнены</a><span className="content__menu__span">187</span></li>
+          <li><a href="/">Отменены</a><span className="content__menu__span">4</span></li>
+          <li><a href="/">Черновики</a><span className="content__menu__span">7</span></li>
         </ul>
       <label className="content__menu__circle">
       <img src="./assets/ic_speeddial_white_24dp_2x.png" alt="" />
